262ac37e6e1a0d1d2705fd77c0475a6136853c9a
GeniusCoder081 <ritikprajapati081@gmail.com>
2024-11-21 22:06:06 +0530

    case-study add in enterprises file

--- Enterprises.php
+++ Enterprises.php
@@ -429,6 +429,234 @@
       </div>
    </div>
 </section>
+<section class="case-study-section">
+   <div class="container">
+      <div class="case-study-wrapper">
+         <div class="case-study-heading-block" style="max-width: 1000px">
+            <h3 class="case-study-heading" style="font-size: 5.8rem">
+            Case study
+            </h3>
+         </div>
+         <div class="case-study-row d-flex flex-wrap justify-content-between">
+            <div class="case-study-box">
+               <a
+                  href="#"
+                  class="sub-case-study-block d-flex flex-column text-decoration-none">
+                  <div
+                     class="image-content video-case-study position-relative d-inline-block overflow-hidden d-flex justify-content-center">
+                     <img
+                        src="./assests/images/Screenshot 2024-11-18 140713.png"
+                        class="img-fluid d-block w-100 wp-post-image"
+                        alt="Stock market learning app"
+                        loading="lazy"
+                        decoding="async" />
+                     <video
+                        muted
+                        playsinline
+                        autoplay
+                        loop
+                        preload="auto"
+                        loading="lazy"
+                        class="case-study-video position-absolute"
+                        src="./assests/video/case001.mp4"></video>
+                  </div>
+               </a>
+               <h4 class="case-sub-title fs1  fs1 pt-lg-2 pt-md-2 pt-2 mt-lg-3 mt-md-3">
+                  Online Plane Tickets booking platform
+               </h4>
+               <a
+                  href="#"
+                  class="case-study-title d-block text-decoration-none position-relative pt-lg-1 pb-lg-1">
+                  AirClass: Business Class Flights & Airline Tickets
+               </a>
+               <p class="case-title-desc">
+                  Experience luxury with AirClass! Book business class flights and premium airline tickets for seamless, comfortable, worldwide travel.
+               </p>
+            </div>
+            <div class="case-study-box">
+               <a
+                  href="#"
+                  class="sub-case-study-block d-flex flex-column text-decoration-none">
+                  <div
+                     class="image-content video-case-study position-relative d-inline-block overflow-hidden">
+                     <img
+                        src="./assests/images/case1-box.webp"
+                        class="img-fluid d-block w-100 wp-post-image"
+                        alt="Stock market learning app"
+                        loading="lazy"
+                        decoding="async" />
+                     <video
+                        muted
+                        playsinline
+                        autoplay
+                        loop
+                        preload="auto"
+                        loading="lazy"
+                        class="case-study-video position-absolute"
+                        src="./assests/video/case-1-video.mp4"></video>
+                  </div>
+               </a>
+               <h4 class="case-sub-title fs1  pt-lg-2 pt-md-2 pt-2 mt-lg-3 mt-md-3">
+                  Mobile app UI UX design
+               </h4>
+               <a
+                  href="#"
+                  class="case-study-title d-block text-decoration-none position-relative pt-lg-1 pb-lg-1">
+                  StockPe: Stock market learning &#038; trading app
+               </a>
+               <p class="case-title-desc">
+                  Design for a Fintech platform that takes a gamified approach to
+                  make stock education and investment simple and engaging.
+               </p>
+            </div>
+            <div class="case-study-box">
+               <a
+                  href="#"
+                  class="sub-case-study-block d-flex flex-column text-decoration-none">
+                  <div
+                     class="image-content video-case-study position-relative d-inline-block overflow-hidden">
+                     <img
+                        src="./assests/images/case-2.webp"
+                        class="img-fluid d-block w-100 wp-post-image"
+                        alt="Stock market learning app"
+                        loading="lazy"
+                        decoding="async" />
+                     <video
+                        muted
+                        playsinline
+                        autoplay
+                        loop
+                        preload="auto"
+                        loading="lazy"
+                        class="case-study-video position-absolute"
+                        src="./assests/video/case-2-video.mp4"></video>
+                  </div>
+               </a>
+               <h4 class="case-sub-title fs1  pt-lg-2 pt-md-2 pt-2 mt-lg-3 mt-md-3">
+                  Ecommerce website design & UI development
+               </h4>
+               <a
+                  href="#"
+                  class="case-study-title d-block text-decoration-none position-relative pt-lg-1 pb-lg-1">
+                  Wire & Switch: Ecommerce platform
+               </a>
+               <p class="case-title-desc">
+                  Design & development for an electrical ecommerce platform to
+                  create a seamless shopping experience & improve sales.
+               </p>
+            </div>
+            <div class="case-study-box">
+               <a
+                  href="#"
+                  class="sub-case-study-block d-flex flex-column text-decoration-none">
+                  <div
+                     class="image-content video-case-study position-relative d-inline-block overflow-hidden">
+                     <img
+                        src="./assests/images/case-3.webp"
+                        class="img-fluid d-block w-100 wp-post-image"
+                        alt="Stock market learning app"
+                        loading="lazy"
+                        decoding="async" />
+                     <video
+                        muted
+                        playsinline
+                        autoplay
+                        loop
+                        preload="auto"
+                        loading="lazy"
+                        class="case-study-video position-absolute"
+                        src="./assests/video/case-3.mp4"></video>
+                  </div>
+               </a>
+               <h4 class="case-sub-title fs1  pt-lg-2 pt-md-2 pt-2 mt-lg-3 mt-md-3">
+                  Web app UX UI design
+               </h4>
+               <a
+                  href="#"
+                  class="case-study-title d-block text-decoration-none position-relative pt-lg-1 pb-lg-1">
+                  IUDX: Data exchange platform
+               </a>
+               <p class="case-title-desc">
+                  Web UI UX design for IUDX, a smart city data exchange platform
+                  under the Govt. of India to facilitate data accessing & sharing.
+               </p>
+            </div>
+            <div class="case-study-box">
+               <a
+                  href="#"
+                  class="sub-case-study-block d-flex flex-column text-decoration-none">
+                  <div
+                     class="image-content video-case-study position-relative d-inline-block overflow-hidden">
+                     <img
+                        src="./assests/images/case-4.webp"
+                        class="img-fluid d-block w-100 wp-post-image"
+                        alt="Stock market learning app"
+                        loading="lazy"
+                        decoding="async" />
+                     <video
+                        muted
+                        playsinline
+                        autoplay
+                        loop
+                        preload="auto"
+                        loading="lazy"
+                        class="case-study-video position-absolute"
+                        src="./assests/video/case-4.mp4"></video>
+                  </div>
+               </a>
+               <h4 class="case-sub-title fs1  pt-lg-2 pt-md-2 pt-2 mt-lg-3 mt-md-3">
+                  Mobile app UI UX design
+               </h4>
+               <a
+                  href="#"
+                  class="case-study-title d-block text-decoration-none position-relative pt-lg-1 pb-lg-1">
+                  Pebbles: Self-help app
+               </a>
+               <p class="case-title-desc">
+                  Mobile app design for a journaling & goal setting platform to
+                  encourage user interactions and increase user engagement.
+               </p>
+            </div>
+            <div class="case-study-box">
+               <a
+                  href="#"
+                  class="sub-case-study-block d-flex flex-column text-decoration-none">
+                  <div
+                     class="image-content video-case-study position-relative d-inline-block overflow-hidden">
+                     <img
+                        src="./assests/images/case-5.webp"
+                        class="img-fluid d-block w-100 wp-post-image"
+                        alt="Stock market learning app"
+                        loading="lazy"
+                        decoding="async" />
+                     <video
+                        muted
+                        playsinline
+                        autoplay
+                        loop
+                        preload="auto"
+                        loading="lazy"
+                        class="case-study-video position-absolute"
+                        src="./assests/video/case-5.mp4"></video>
+                  </div>
+               </a>
+               <h4 class="case-sub-title fs1  pt-lg-2 pt-md-2 pt-2 mt-lg-3 mt-md-3">
+                  Mobile app design & UI development
+               </h4>
+               <a
+                  href="#"
+                  class="case-study-title d-block text-decoration-none position-relative pt-lg-1 pb-lg-1">
+                  DAMAC: Real estate agents &#038; application
+               </a>
+               <p class="case-title-desc">
+                  UX UI redesign and development for a leading real estate
+                  enterprise to improve their agents’ operations & communication.
+               </p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
 <section class="insight-section">
    <div class="container-insight">
       <div class="insight-head-box d-flex justify-content-between">
